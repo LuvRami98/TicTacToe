e0b67889adfaebb0d753b24ee0013683f8c3cd82
LuvRami98 <harald.vonheijne@gmail.com>
2024-11-05 22:13:33 +0100

    Create Player1.cs

---
+++ Models/Player1.cs
@@ -0,0 +1,10 @@
+using MongoDB.Bson;
+
+namespace _2Övningsuppgifter2.Models
+{
+    public class Player1
+    {
+        public ObjectId id { get; set; }
+        public string CrossOne { get; set; }
+    }
+}
