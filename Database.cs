0eda9e896455286bf9bec3f8283d891ba50e15b2
LuvRami98 <harald.vonheijne@gmail.com>
2024-11-05 22:00:57 +0100

    Add files via upload

---
+++ Database.cs
@@ -0,0 +1,20 @@
+﻿using _2Övningsuppgifter2.Models;
+using MongoDB.Driver;
+
+namespace _2Övningsuppgifter2
+{
+    public class Database
+    {
+        private IMongoDatabase GetDB()
+        {
+            MongoClient client = new MongoClient();
+            var db = client.GetDatabase("PlayerDB");
+            return db;
+        }
+
+        public async Task SavePlay(Player1 game)
+        {
+            await GetDB().GetCollection<Player1>("Play").InsertOneAsync(game);
+        }
+    }
+}
---
+++ DatabaseTwo.cs
@@ -0,0 +1,20 @@
+﻿using _2Övningsuppgifter2.Models;
+using MongoDB.Driver;
+
+namespace _2Övningsuppgifter2
+{
+    public class DatabaseTwo
+    {
+        private IMongoDatabase GetDB()
+        {
+            MongoClient client = new MongoClient();
+            var db = client.GetDatabase("PlayerDB");
+            return db;
+        }
+
+        public async Task SavePlay2(Player2 gamePlay)
+        {
+            await GetDB().GetCollection<Player2>("PlayGame").InsertOneAsync(gamePlay);
+        }
+    }
+}
