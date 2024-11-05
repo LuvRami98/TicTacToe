d97c2061f13ff5720e9b3f27c5ddee66b3584452
LuvRami98 <harald.vonheijne@gmail.com>
2024-11-05 22:14:09 +0100

    Create Player2.cs

---
+++ Models/Player2.cs
@@ -0,0 +1,10 @@
+using MongoDB.Bson;
+
+namespace _2Övningsuppgifter2.Models
+{
+    public class Player2
+    {
+        public ObjectId id { get; set; }
+        public string CircleTwo { get; set; }
+    }
+}
